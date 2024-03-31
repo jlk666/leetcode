f61215e054b7cc7bc7e2ba146cd9a2c0830818c1
jlk666 <zsliu@ucdavis.edu>
2024-03-31 00:01:48 -0700

    Two sum using two data structure type added

--- twoPointer/TwoSum.py
+++ twoPointer/TwoSum.py
@@ -1,4 +1,4 @@
-class TwoSum():
+class TwoSum_listbased():
     def __init__(self) :
         self.num_list = []
     
@@ -9,4 +9,23 @@ class TwoSum():
             self.num_list[index_num -1], self.num_list[index_num] = self.num_list[index_num], self.num_list[index_num -1]
             index_num -= 1
             
-             
+    def find(self, target): #using two num pointers
+        left = 0
+        right = len(self.num_list)-1
+        while left < right:
+            if self.num_list[left] + self.num_list[right] == target:
+                return True
+            if self.num_list[left] + self.num_list[right] < target:
+                left+=1
+            if self.num_list[left] + self.num_list[right] > target:
+                right-=1
+        return False
+
+class TwoSum_hashMapbased():
+    def __init__(self) :
+        self.num_hash = {}
+        
+    def add(self, number):
+        self.num_hash[number] = self.num_hash.get(number, 0) + 1 # the 2nd parameter here, if there is no number in the hash, set the value to 0
+                
+            
