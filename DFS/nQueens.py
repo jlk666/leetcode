0aef9d09bee1b3d5fab555bd37970dc34358dcca
jlk666 <zsliu@ucdavis.edu>
2024-06-19 22:03:58 -0700

    Update nQueens.py
    isValid function added

--- DFS/nQueens.py
+++ DFS/nQueens.py
@@ -7,16 +7,24 @@ class Solution(object):
         results = []
         self.search(n, [], results)
         return results
+    
     def search(self, n, cols, results):
         row = len(cols)
         if row == n:
             results.append()
             return 
         for col in range (n):
-            if not isValid(cols, col, row):
+            if not self.isValid(cols, col, row):
                 continue
             cols.append(col)
             self.search(n, cols, results)
             cols.pop()
             
-        
+    def isValid(cols, col, row):
+        for r, c in enumerate(cols):
+            if c == col:
+                return False
+            if r + c == col+row or r - c == col - row:
+                return False
+        return True
+    
