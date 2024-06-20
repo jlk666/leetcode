3b0879c35f0add53c16c92c03c45dc6e09332020
jlk666 <zsliu@ucdavis.edu>
2024-06-19 22:25:00 -0700

    answer uploaded
    answer using DFS

--- DFS/nQueens.py
+++ DFS/nQueens.py
@@ -11,7 +11,7 @@ class Solution(object):
     def search(self, n, cols, results):
         row = len(cols)
         if row == n:
-            results.append()
+            results.append(self.printboard(cols))
             return 
         for col in range (n):
             if not self.isValid(cols, col, row):
@@ -20,11 +20,18 @@ class Solution(object):
             self.search(n, cols, results)
             cols.pop()
             
-    def isValid(cols, col, row):
+    def isValid(self, cols, col, row):
         for r, c in enumerate(cols):
             if c == col:
                 return False
-            if r + c == col+row or r - c == col - row:
+            if r + c == col+row or r - c == row - col:
                 return False
         return True
     
+    def printboard(self, cols):
+        n = len(cols)
+        board = []
+        for i in range(n):
+            row = ['Q' if j ==cols[i] else '.' for j in range(n)]
+            board.append(''.join(row))
+        return board
