986df20f7761e67dc093e97794920be69b0d389e
Zhuosheng Liu <78391143+jlk666@users.noreply.github.com>
2024-06-25 23:03:30 -0700

    Update letterCombination.py
    intro added

--- DFS/letterCombination.py
+++ DFS/letterCombination.py
@@ -4,4 +4,12 @@ class Solution(object):
         :type digits: str
         :rtype: List[str]
         """
-        
+        Keyboard = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+                    '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+    
+    def dfs(self, digits, index, path, res):
+        if index == len(digits):
+            res.append(path)
+            return
+        for letter in self.dict[digits[index]]:
+            self.dfs(digits, index + 1, path + i, res)
