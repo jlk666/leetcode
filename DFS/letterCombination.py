985981f3f590e72a82ed1bbf50ac7fd23c1a892a
Zhuosheng Liu <78391143+jlk666@users.noreply.github.com>
2024-06-28 21:49:45 -0700

    Update letterCombination.py

--- DFS/letterCombination.py
+++ DFS/letterCombination.py
@@ -4,14 +4,20 @@ class Solution(object):
         :type digits: str
         :rtype: List[str]
         """
-        Keyboard = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+        self.Keyboard = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
                     '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+        
+        if not digits:
+            return []
+        recombinations = []
+        self.dfs(digits, 0, [], recombinations)
+        return recombinations
     
-    def dfs(self, digits, index, path, res):
+    def dfs(self, digits, index, combination, combinations):
         if index == len(digits):
-            res.append(path)
+            combinations.append(''.join(combination))
             return
-        for letter in self.dict[digits[index]]:
-            path.append(letter)
-            self.dfs(digits, index + 1, path , res)
-            path.pop()
+        for letter in self.Keyboard[digits[index]]:
+            combination.append(letter)
+            self.dfs(digits, index + 1, combination, combinations)
+            combination.pop()
